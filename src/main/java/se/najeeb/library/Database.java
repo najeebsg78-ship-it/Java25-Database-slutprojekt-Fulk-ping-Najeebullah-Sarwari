c534d3e4083403cde6ad84d748990794e359e686
najeebsg78-ship-it <najeebsg78@gmail.com>
2026-03-03 22:08:42 +0100

    skapade Database klass

---
+++ src/main/java/se/najeeb/library/Database.java
@@ -0,0 +1,34 @@
+package se.najeeb.library;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+
+    private static Connection db = null;
+
+    private Database(){};
+
+    private static Connection getConnection (){
+
+        if (db != null) return db;
+
+        String username = "root";
+        String password = "rootpassword";
+        String host = "localhost";
+        String port = "3306";
+        String database = "LIBRARY";
+
+        try {
+            String url = "jdbc:mysql://" + host + ( port.isBlank() ? "" : ":" + port);
+            url += (database.isBlank() ? "/" : "/" + database) + "?useSSL=false";
+
+            db = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return db;
+    }
+}
